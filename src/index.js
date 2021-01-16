90143fe32f8e3676d7290ab2995a42e4d1992a81
Jeffrey Martinez <57267088+martinezjf2@users.noreply.github.com>
2021-01-15 22:12:44 -0500

    es6 template literal

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = "Jeffrey";
+const lname = "Martinez";
 const number = 7;
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>Hello {`${name} ${lname}`}!</h1>
     <p>Your lucky number is {number}!</p>
   </div>,
   document.getElementById("root")
